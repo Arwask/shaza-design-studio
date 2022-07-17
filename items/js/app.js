a79e80cde1b2b42ee6666e091c7a70c838322480
Arwask <Arwa.it15@gmail.com>
2022-07-17 10:13:08 -0400

    adding code to dynaically load the images for residential for now

--- items/js/app.js
+++ items/js/app.js
@@ -28,3 +28,21 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+resImages=['E_MKGs_HR-1 (1).jpg','E_MKGs_HR-2 (1).jpg', 'E_MKGs_HR-2.jpg', 'E_MKGs_HR-3.jpg', 'E_MKGs_HR-3 (1).jpg', 'E_MKGs_HR-6-2.jpg', 'E_MKGs_HR-7.jpg', 'E_MKGs_HR-11.jpg','E_MKGs_LR-10.jpg', 'E_MKGs_LR-9.jpg', 'k3.png']
+window.onload = () => {
+  for(let i=0;i<resImages.length;i++){
+    // let list = document.createElement("div");
+    // list.setAttribute("class","project-card");
+    // list.setAttribute("class", "project-container")
+    let img = document.createElement("img")
+    img.setAttribute("src",`items/images/${resImages[i]}`);
+    img.setAttribute("alt", "residential images");
+    img.setAttribute("loading", "lazy");
+    img.setAttribute("class", "project-pic")
+    // list.appendChild(img)
+    document.getElementById('res-parent-div').appendChild(img);
+  }
+
+
+}
