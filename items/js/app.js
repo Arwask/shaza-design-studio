05bcdc8d303d05e605fee7700b536312f30b8a12
Arwask <Arwa.it15@gmail.com>
2022-07-18 11:29:06 -0400

    social links, projects link done

--- items/js/app.js
+++ items/js/app.js
@@ -29,20 +29,46 @@ navLink.forEach((link) =>
   })
 );
 
-resImages=['E_MKGs_HR-1 (1).jpg','E_MKGs_HR-2 (1).jpg', 'E_MKGs_HR-2.jpg', 'E_MKGs_HR-3.jpg', 'E_MKGs_HR-3 (1).jpg', 'E_MKGs_HR-6-2.jpg', 'E_MKGs_HR-7.jpg', 'E_MKGs_HR-11.jpg','E_MKGs_LR-10.jpg', 'E_MKGs_LR-9.jpg', 'k3.png']
-window.onload = () => {
+let resImages=['E_MKGs_HR-1 (1).jpg','E_MKGs_HR-2 (1).jpg', 'E_MKGs_HR-2.jpg', 'E_MKGs_HR-3.jpg', 'E_MKGs_HR-3 (1).jpg', 'E_MKGs_HR-6-2.jpg', 'E_MKGs_HR-7.jpg', 'E_MKGs_HR-11.jpg','E_MKGs_LR-10.jpg', 'E_MKGs_LR-9.jpg', 'k3.png']
+let comImages=[];
+let threeDImages=[];
+
+function loadResImages() {
   for(let i=0;i<resImages.length;i++){
-    // let list = document.createElement("div");
-    // list.setAttribute("class","project-card");
-    // list.setAttribute("class", "project-container")
     let img = document.createElement("img")
     img.setAttribute("src",`items/images/${resImages[i]}`);
     img.setAttribute("alt", "residential images");
     img.setAttribute("loading", "lazy");
     img.setAttribute("class", "project-pic")
-    // list.appendChild(img)
     document.getElementById('res-parent-div').appendChild(img);
   }
+}
 
+function loadComImages() {
+  for(let i=0;i<comImages.length;i++){
+    let img = document.createElement("img")
+    img.setAttribute("src",`items/images/${resImages[i]}`);
+    img.setAttribute("alt", "residential images");
+    img.setAttribute("loading", "lazy");
+    img.setAttribute("class", "project-pic")
+    document.getElementById('com-parent-div').appendChild(img);
+  }
+}
+
+function load3DImages() {
+
+  for(let i=0;i<threeDImages.length;i++){
+    let img = document.createElement("img")
+    img.setAttribute("src",`items/images/${resImages[i]}`);
+    img.setAttribute("alt", "residential images");
+    img.setAttribute("loading", "lazy");
+    img.setAttribute("class", "project-pic")
+    document.getElementById('3d-parent-div').appendChild(img);
+  }
+}
+window.onload = () => {
+  (resImages.length > 0) ? loadResImages() : (document.getElementById('res-h2').setAttribute('class',"displayh2"));
+  (comImages.length > 0) ? loadComImages() : (document.getElementById('com-h2').setAttribute('class',"displayh2"));
+  (threeDImages.length > 0) ? load3DImages(): (document.getElementById('3d-h2').setAttribute('class',"displayh2"));
 
 }
